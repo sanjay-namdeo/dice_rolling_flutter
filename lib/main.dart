8576c1db3d8feac2e1b69103b29c5d6457347da9
Sanjay Namdeo <sanjay.namdeo@live.com>
2019-05-18 11:33:59 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        appBar: AppBar(
+          backgroundColor: Colors.teal.shade700,
+          title: Text('Dice Rolling'),
+        ),
+        body: Container(),
+      ),
+    );
+  }
+}
